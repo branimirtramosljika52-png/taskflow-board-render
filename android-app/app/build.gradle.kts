e47ae6e5b4bb72b4b0a3717fc996c6e70c393de6
Branimir <branimir@example.com>
2026-07-08 09:49:08 +0200

    Serve release Android APK

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.safenexus.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 355
-        versionName = "0.1.357"
+        versionCode = 356
+        versionName = "0.1.358"
 
         buildConfigField(
             "String",
